0c0575cecb65f6b785a619a55e68eeac6b1f60ba
unknown <tuvantranhttta@gmail.com>
2021-03-06 15:04:14 +0700

    Khoi tao

---
+++ application/views/layout/footer.php
@@ -0,0 +1,98 @@
+<footer class="footer-section">
+<div class="container">
+<div class="row">
+<div class="col-lg-3">
+<div class="footer-left">
+<div class="footer-logo">
+<a href="#"><img src="img/footer-logo.png" alt=""></a>
+</div>
+<ul>
+<li>Address: 60-49 Road 11378 New York</li>
+<li>Phone: +65 11.188.888</li>
+<li>Email: <a href="https://preview.colorlib.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="d1b9b4bdbdbeffb2bebdbea3bdb8b391b6bcb0b8bdffb2bebc">[email&#160;protected]</a></li>
+</ul>
+<div class="footer-social">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-instagram"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-pinterest"></i></a>
+</div>
+</div>
+</div>
+<div class="col-lg-2 offset-lg-1">
+<div class="footer-widget">
+<h5>Information</h5>
+<ul>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Checkout</a></li>
+<li><a href="#">Contact</a></li>
+<li><a href="#">Serivius</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-2">
+<div class="footer-widget">
+ <h5>My Account</h5>
+<ul>
+<li><a href="#">My Account</a></li>
+<li><a href="#">Contact</a></li>
+<li><a href="#">Shopping Cart</a></li>
+<li><a href="#">Shop</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="newslatter-item">
+<h5>Join Our Newsletter Now</h5>
+<p>Get E-mail updates about our latest shop and special offers.</p>
+<form action="#" class="subscribe-form">
+<input type="text" placeholder="Enter Your Mail">
+<button type="button">Subscribe</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+<div class="copyright-reserved">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="copyright-text">
+
+Copyright &copy;<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com/" target="_blank">Colorlib</a>
+
+</div>
+<div class="payment-pic">
+<img src="img/payment-method.png" alt="">
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+
+
+<script src="<?= base_url('assets/js/jquery-3.3.1.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery-ui.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery.countdown.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery.nice-select.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery.zoom.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery.dd.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery.slicknav.js'); ?>"></script>
+<script src="<?= base_url('assets/js/owl.carousel.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/main.js'); ?>"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="../../../static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"rayId":"62b974b529dbc755","si":10,"version":"2021.2.0"}'></script>
+</body>
+
+<!-- Mirrored from preview.colorlib.com/theme/fashi/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 06 Mar 2021 06:16:59 GMT -->
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- Mirrored from preview.colorlib.com/theme/fashi/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 06 Mar 2021 06:16:34 GMT -->
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="Fashi Template">
+<meta name="keywords" content="Fashi, unica, creative, html">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Fashi | Template</title>
+
+<link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/font-awesome.min.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/themify-icons.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/elegant-icons.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/owl.carousel.min.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/nice-select.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/jquery-ui.min.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/slicknav.min.css');?> " type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/style.css');?> " type="text/css">
+</head>
+<body>
+
+<div id="preloder">
+<div class="loader"></div>
+</div>
+
+<header class="header-section">
+<div class="header-top">
+<div class="container">
+<div class="ht-left">
+<div class="mail-service">
+<i class=" fa fa-envelope"></i>
+<a href="https://preview.colorlib.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="e58d8089898acb868a898a97898c87a58288848c89cb868a88">[email&#160;protected]</a>
+</div>
+<div class="phone-service">
+<i class=" fa fa-phone"></i>
++65 11.188.888
+</div>
+</div>
+<div class="ht-right">
+<a href="#" class="login-panel"><i class="fa fa-user"></i>Login</a>
+<div class="lan-selector">
+<select class="language_drop" name="countries" id="countries" style="width:300px;">
+<option value='yt' data-image="img/flag-1.jpg" data-imagecss="flag yt" data-title="English">English</option>
+<option value='yu' data-image="img/flag-2.jpg" data-imagecss="flag yu" data-title="Bangladesh">German </option>
+</select>
+</div>
+<div class="top-social">
+<a href="#"><i class="ti-facebook"></i></a>
+<a href="#"><i class="ti-twitter-alt"></i></a>
+<a href="#"><i class="ti-linkedin"></i></a>
+<a href="#"><i class="ti-pinterest"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="inner-header">
+<div class="row">
+<div class="col-lg-2 col-md-2">
+<div class="logo">
+<a href="index-2.html">
+<img src="img/logo.png" alt="">
+</a>
+</div>
+</div>
+<div class="col-lg-7 col-md-7">
+<div class="advanced-search">
+<button type="button" class="category-btn">All Categories</button>
+<div class="input-group">
+<input type="text" placeholder="What do you need?">
+<button type="button"><i class="ti-search"></i></button>
+</div>
+</div>
+</div>
+<div class="col-lg-3 text-right col-md-3">
+<ul class="nav-right">
+<li class="heart-icon">
+<a href="#">
+<i class="icon_heart_alt"></i>
+<span>1</span>
+</a>
+</li>
+<li class="cart-icon">
+<a href="#">
+<i class="icon_bag_alt"></i>
+<span>3</span>
+</a>
+<div class="cart-hover">
+<div class="select-items">
+<table>
+<tbody>
+<tr>
+<td class="si-pic"><img src="img/select-product-1.jpg" alt=""></td>
+<td class="si-text">
+<div class="product-selected">
+<p>$60.00 x 1</p>
+<h6>Kabino Bedside Table</h6>
+</div>
+</td>
+ <td class="si-close">
+<i class="ti-close"></i>
+</td>
+</tr>
+<tr>
+<td class="si-pic"><img src="img/select-product-2.jpg" alt=""></td>
+<td class="si-text">
+<div class="product-selected">
+<p>$60.00 x 1</p>
+<h6>Kabino Bedside Table</h6>
+</div>
+</td>
+<td class="si-close">
+<i class="ti-close"></i>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+<div class="select-total">
+<span>total:</span>
+<h5>$120.00</h5>
+</div>
+<div class="select-button">
+<a href="#" class="primary-btn view-card">VIEW CARD</a>
+<a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
+</div>
+</div>
+</li>
+<li class="cart-price">$150.00</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="nav-item">
+<div class="container">
+<div class="nav-depart">
+<div class="depart-btn">
+<i class="ti-menu"></i>
+<span>All departments</span>
+<ul class="depart-hover">
+<li class="active"><a href="#">Women’s Clothing</a></li>
+<li><a href="#">Men’s Clothing</a></li>
+<li><a href="#">Underwear</a></li>
+<li><a href="#">Kid's Clothing</a></li>
+<li><a href="#">Brand Fashion</a></li>
+<li><a href="#">Accessories/Shoes</a></li>
+<li><a href="#">Luxury Brands</a></li>
+<li><a href="#">Brand Outdoor Apparel</a></li>
+</ul>
+</div>
+</div>
+<nav class="nav-menu mobile-menu">
+<ul>
+<li class="active"><a href="index-2.html">Home</a></li>
+<li><a href="shop.html">Shop</a></li>
+<li><a href="#">Collection</a>
+<ul class="dropdown">
+<li><a href="#">Men's</a></li>
+<li><a href="#">Women's</a></li>
+<li><a href="#">Kid's</a></li>
+</ul>
+</li>
+<li><a href="blog.html">Blog</a></li>
+<li><a href="contact.html">Contact</a></li>
+<li><a href="#">Pages</a>
+<ul class="dropdown">
+<li><a href="blog-details.html">Blog Details</a></li>
+<li><a href="shopping-cart.html">Shopping Cart</a></li>
+<li><a href="check-out.html">Checkout</a></li>
+<li><a href="faq.html">Faq</a></li>
+<li><a href="register.html">Register</a></li>
+<li><a href="login.html">Login</a></li>
+</ul>
+</li>
+</ul>
+</nav>
+<div id="mobile-menu-wrap"></div>
+</div>
+</div>
+</header>
